44c1a9883f97ea7305acc49c958b71efb0764e20
victorzarzar <victorzarzar58@gmail.com>
2025-06-22 15:30:46 -0300

    Update layout and report generator.

--- src/charts/chart_generator.py
+++ src/charts/chart_generator.py
@@ -3,11 +3,41 @@ import os
 
 
 def generate_pie_chart(df):
-    grouped = df.groupby('categoria')['valor'].sum()
-    fig, ax = plt.subplots()
-    ax.pie(grouped, labels=grouped.index, autopct='%1.1f%%')
-    plt.title('Distribuição de Gastos por Categoria')
+    # Delete recipes
+    df = df[~df['categoria'].str.lower().str.contains('salário|adicional')]
+
+    # Group values ​​by category
+    grouped = df.groupby('categoria')[
+        'valor'].sum().sort_values(ascending=False)
+
+    total = grouped.sum()
+    threshold = 0.04 * total
+    grouped_filtered = grouped[grouped >= threshold]
+    others = grouped[grouped < threshold].sum()
+    if others > 0:
+        grouped_filtered["Outros"] = others
+
+    labels = grouped_filtered.index
+    values = grouped_filtered.values
+
+    # Chart
+    fig, ax = plt.subplots(figsize=(8, 6))
+    wedges, texts, autotexts = ax.pie(
+        values,
+        labels=labels,
+        autopct=lambda pct: f'{pct:.1f}%\nR${pct * total / 100:.0f}',
+        startangle=140,
+        colors=plt.cm.tab20.colors[:len(labels)],
+        textprops=dict(color="black", fontsize=9)
+    )
+
+    ax.axis('equal')
+    plt.title('Distribuição de Gastos por Categoria', fontsize=12)
+    plt.tight_layout()
+
+    # File path
     output_path = 'reports/pie_chart.png'
     plt.savefig(output_path)
     plt.close()
+
     return output_path
--- src/reports/report_generator.py
+++ src/reports/report_generator.py
@@ -4,27 +4,69 @@ from reportlab.lib.styles import getSampleStyleSheet
 from datetime import datetime
 
 
+# Categories considered variable expenses
+CATEGORIES_VARIABLES = [
+    "Aluguel", "Investimentos", "Impostos", "Saúde", "Lazer", "Gastos Extras"
+]
+
+# Categories considered fixed income
+CATEGORIES_FIXED_REVENUE = ["Salário"]
+
+
 def generate_pdf(df, chart_path):
     doc = SimpleDocTemplate("reports/expenses_2025_07.pdf", pagesize=A4)
     styles = getSampleStyleSheet()
     story = []
 
-    total = df['valor'].sum()
+    def classificar_linha(row):
+        if row['categoria'] in CATEGORIES_FIXED_REVENUE:
+            return 'receita_fixa'
+        elif row['categoria'] == "Adicionais":
+            return 'receita_variavel'
+        elif row['categoria'] in CATEGORIES_VARIABLES:
+            return 'despesa_variavel'
+        else:
+            return 'despesa_fixa'
+
+    df['tipo'] = df.apply(classificar_linha, axis=1)
+
+    receitas_fixas = df[df['tipo'] == 'receita_fixa']['valor'].sum()
+    receitas_variaveis = df[df['tipo'] == 'receita_variavel']['valor'].sum()
+    despesas_fixas = df[df['tipo'] == 'despesa_fixa']['valor'].sum()
+    despesas_variaveis = df[df['tipo'] == 'despesa_variavel']['valor'].sum()
+
+    saldo = (receitas_fixas + receitas_variaveis) - \
+        (despesas_fixas + despesas_variaveis)
+
     grouped = df.groupby('categoria')['valor'].sum()
 
     story.append(Paragraph("Relatório de Gastos Mensais", styles['Title']))
     story.append(Paragraph(
         f"Data de geração: {datetime.now().strftime('%d/%m/%Y')}", styles['Normal']))
-    story.append(Paragraph(f"Total: R$ {total:,.2f}", styles['Normal']))
     story.append(Spacer(1, 12))
 
-    story.append(Paragraph("Resumo por categoria:", styles['Heading2']))
+    # Resume
+    story.append(Paragraph("Resumo Financeiro:", styles['Heading2']))
+    story.append(
+        Paragraph(f"Receitas Fixas: R$ {receitas_fixas:,.2f}", styles['Normal']))
+    story.append(Paragraph(
+        f"Receitas Variáveis: R$ {receitas_variaveis:,.2f}", styles['Normal']))
+    story.append(
+        Paragraph(f"Despesas Fixas: R$ {despesas_fixas:,.2f}", styles['Normal']))
+    story.append(Paragraph(
+        f"Despesas Variáveis: R$ {despesas_variaveis:,.2f}", styles['Normal']))
+    story.append(
+        Paragraph(f"<b>Saldo Final: R$ {saldo:,.2f}</b>", styles['Normal']))
+    story.append(Spacer(1, 12))
+
+    story.append(Paragraph("Resumo por Categoria:", styles['Heading2']))
     for categoria, valor in grouped.items():
         story.append(
             Paragraph(f"{categoria}: R$ {valor:.2f}", styles['Normal']))
-
     story.append(Spacer(1, 24))
-    story.append(Paragraph("Gráfico:", styles['Heading2']))
+
+    # Chart
+    story.append(Paragraph("Gráfico de Gastos:", styles['Heading2']))
     story.append(Image(chart_path, width=400, height=300))
 
     doc.build(story)
